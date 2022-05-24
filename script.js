32acff689cd3ec042d129ddbb40b44ebd2ea47da
Eric Chagrikian <echagrikian@hotmail.com>
2022-05-24 12:21:09 +0200

    suppression commentaires

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-
-
-// function getEvents() {
-// fetch('http://localhost:3000/api/events/')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
-
-//     for (let i = 0; i < data.length; i++) {
-//         const dates = data[i].dates;
-        
-//     }
-// };
-// getEvents()
-
-
- async function loadDatesIntoHead() {
+async function loadDatesIntoHead() {
      const response = await fetch('http://localhost:3000/api/events/');
      const info = await response.json();
      console.log(info);
@@ -109,50 +94,4 @@
 
 }
 
-// async function loadNamesIntoBody(url) {
-//      let selectTable = document.queryselector('table');
-//      let tableBody = document.createElement('tbody');
-//      selectTable.appendChild(tableBody);
-//      
-//      const response = await fetch(url);
-//      const info = await response.json();
-
-//      console.log(info);
-// };  
-
-
-
-// async function loadIntoTable(url, table) {
-//     const tableH = table.querySelector('thead')
-//     const tableB = table.querySelector('tbody')
-//     const response = await fetch(url)
-//     const dates = await response.json();
-//     tableH.innerHTML = "<tr></tr>";
-//     tableB.innerHTML = "";
-//     console.log(events)
-
-//     for (const headerText of dates) {
-//         const headerElement = document.createElement("th");
-
-//         headerElement.textContent = headerText
-//         tableHead.querySelector("tr").appendChild(headerElement);
-//     }
-
-//     for (const row of names) {
-//         const rowElement = document.createElement("tr");
-
-//         for (const cellText of row) {
-//             const cellElement = document.createElement("td");
-
-//             cellElement.textContent = cellText;
-//             rowElement.appendChild(cellElement);
-//         }
-
-//         tableBody.appendChild(rowElement)
-//     }
-// }
-
-// loadIntoTable("http://localhost:3000/", document.querySelector('table'));
 loadDatesIntoHead();
-
-
